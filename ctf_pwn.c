87115ccd4e7fa8d019c3744944a1bcb7b33f8d85
cloudkevin <cloudkevin@users.noreply.github.com>
2020-01-02 13:43:38 -0700

    Update ctf_pwn.c

--- ctf_pwn.c
+++ ctf_pwn.c
@@ -4,7 +4,7 @@
 
 void __attribute__ ((constructor)) initLibrary(void) {
 	printf("Escabe lib is initialized\n");
-	printf("[LO] uid:%d | euid:%d\n" , getuid(), geteuid());
+	printf("[+] uid:%d | euid:%d\n" , getuid(), geteuid());
 	setuid(1069);
-	printf("[LO] uid:%d | euid:%d\n" , getuid(), geteuid());
+	printf("[+] uid:%d | euid:%d\n" , getuid(), geteuid());
 }
